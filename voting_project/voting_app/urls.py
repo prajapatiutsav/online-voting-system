57626a1c932652e64940cd28a93da4cdba6790b1
Utsav prajapati <149518655+prajapatiutsav@users.noreply.github.com>
2026-03-25 10:58:21 +0530

    Update urls.py

--- voting_project/voting_app/urls.py
+++ voting_project/voting_app/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('register/', views.register, name='register'),
     path('logout/', views.user_logout, name='logout'),
-    path('vote/', views.vote, name='vote'),       # ✅
+
+    path('vote/', views.vote, name='vote'),
     path('results/', views.results, name='results'),
     path('success/', views.success, name='success'),
 ]
